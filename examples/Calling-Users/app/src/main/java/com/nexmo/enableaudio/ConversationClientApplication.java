c0dd28de5a9a019fdc63a2933d559ab802ae4df7
Chris Guzman <guzmantchris@gmail.com>
2018-05-11 14:41:16 -0500

    Set up from Audio Quickstart

---
+++ examples/Calling-Users/app/src/main/java/com/nexmo/enableaudio/ConversationClientApplication.java
@@ -0,0 +1,20 @@
+package com.nexmo.enableaudio;
+
+import android.app.Application;
+
+import com.nexmo.sdk.conversation.client.ConversationClient;
+
+public class ConversationClientApplication extends Application {
+
+    private ConversationClient conversationClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        conversationClient = new ConversationClient.ConversationClientBuilder().context(this).build();
+    }
+
+    public ConversationClient getConversationClient() {
+        return conversationClient;
+    }
+}
